3847986158c9518670956920a4086dfe3f882998
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2020-01-08 15:09:51 -0500

    Wire up a Styled Components example

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
 import { NextPage } from 'next'
+import styled from 'styled-components'
 
 interface TimelineProps {}
 
 const HomePage: NextPage<TimelineProps> = () => {
-  return <h1>Timeline</h1>
+  return <Title>Timeline</Title>
 }
 
 export default HomePage
+
+const Title = styled.h1`
+  color: ${({ theme }) => theme.colors.primary};
+`
