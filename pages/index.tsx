49908c49b1705ba3940750fca75a15dbd194dd9c
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2020-01-08 21:33:41 -0500

    Touch up the index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import { NextPage } from 'next'
 import Head from 'next/head'
 import styled from 'styled-components'
-import { Button } from '@artsy/palette'
+import { Sans, space } from '@artsy/palette'
+import entries from '../data/entriesList.json'
 
 interface TimelineProps {}
 
@@ -11,16 +12,34 @@ const HomePage: NextPage<TimelineProps> = () => {
       <Head>
         <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
       </Head>
-      <div>
-        <Title>Timeline</Title>
-        <Button onClick={() => alert('Hello!')}>Hello, Palette</Button>
-      </div>
+      <style jsx global>{`
+        // minimal css reset
+        * {
+          box-sizing: border-box;
+          margin: 0;
+          padding: 0;
+        }
+        p,
+        ul {
+          margin: 0.5em 0;
+        }
+        li {
+          margin-left: 1.5em;
+        }
+      `}</style>
+      <Main>
+        <Sans size="16" my={3}>
+          Artsy History
+        </Sans>
+
+        <pre>{JSON.stringify(entries, null, 2)}</pre>
+      </Main>
     </>
   )
 }
 
 export default HomePage
 
-const Title = styled.h1`
-  color: ${({ theme }) => theme.colors.primary};
+const Main = styled.main`
+  margin: ${space(2)}px;
 `
